283276c3e80f1154ae43bbd0c15646ecf51b8bbb
Andrew Turner <aturner@esri.com>
2015-05-05 22:36:11 -0400

    Update citysdk.arcgis.js

--- js/citysdk.arcgis.js
+++ js/citysdk.arcgis.js
@@ -1,8 +1,8 @@
 /**
- * CitySDK module for GeoServices served from ArcGIS
- * 
- * Part of the US Census CitySDK project: https://github.com/uscensusbureau/citysdk
- */
+* CitySDK module for GeoServices served from ArcGIS
+* 
+* Part of the US Census CitySDK project: https://github.com/uscensusbureau/citysdk
+*/
 
 //Attach a new module object to the CitySDK prototype.
 //It is advised to keep the filenames and module property names the same
@@ -10,22 +10,24 @@ CitySDK.prototype.modules.arcgis = new ArcgisModule();
 
 //Module object definition. Every module should have an "enabled" property and an "enable"  function.
 function ArcgisModule() {
-    this.enabled = false;
-};
-
-//Create a new connection to a GeoService URL
-ArcgisModule.prototype.create = function(url) {
-    this.url = url;
-    this.enabled = true;
+  this.enabled = true;
 };
 
 /**
- * Processes a data request by looking at a JSON request
- *
- * @param {object} request The JSON object of the request
- * @param {function} callback A callback, which accepts a response parameter
- */
+* Processes a data request by looking at a JSON request
+*
+* @param {object} request The JSON object of the request
+* @param {function} callback A callback, which accepts a response parameter
+*/
 ArcgisModule.prototype.APIRequest = function(request, callback) {
-  callback(request);
-  return
+  var request = CitySDK.prototype.sdkInstance.ajaxRequest(request.url);
+
+  //Attach a completion event to the promise
+  request.done(function(response) {
+    //Turn it into json
+    var jsonObject = $.parseJSON(response);
+    //Call the callback
+    callback(jsonObject);
+  });    
+  return;
 };
