16f859eab33f417677d35dd9a86b088e3301ca1b
ajturner <aturner@esri.com>
2015-05-16 23:39:05 -0400

    Extend CitySDK ArcGIS with spatial and where queries

--- js/citysdk.arcgis.js
+++ js/citysdk.arcgis.js
@@ -14,13 +14,43 @@ function ArcgisModule() {
 };
 
 /**
-* Processes a data request by looking at a JSON request
+* Query an ArcGIS Server
 *
-* @param {object} request The JSON object of the request
-* @param {function} callback A callback, which accepts a response parameter
+* Based on http://resources.arcgis.com/en/help/arcgis-rest-api/index.html#/Query_Feature_Service_Layer/02r3000000r1000000/
+* 
+* spatial filters can include {lat: , lng: }, {extent: [west,south,east,north]}, {geometry: esriPolygon}
+* where filters are based on the service description
+* 
+* @param {object} parameters The JSON object of the request
+* @param {function} callback A callback function which will recieve the query response
 */
-ArcgisModule.prototype.APIRequest = function(request, callback) {
-  var url = request.url.replace(/(Server\/\d+).*$/,'$1/query?f=json&where=1=1&outFields=*');
+ArcgisModule.prototype.APIRequest = function(parameters, callback) {
+  var query = ["f=json","outSR=4326"]
+  query.push("outFields=" + (parameters.outFields || "*"))
+
+  if( parameters.where !== null && parameters.where !== undefined ) {
+    var where = parameters.where;
+    if( Array.isArray(where) ) {
+      where = where.join(" AND ");
+    }
+    query.push("where=" + where);
+  } else {
+    query.push("where=1=1");
+  }
+
+  if( parameters.lat !== undefined && parameters.lng !== undefined ) {
+    query.push("geometryType=esriGeometryPoint&geometry=" + parameters.lng + "," + parameters.lat);  
+  } else if( parameters.extent !== undefined ) { // [W,S,E,N]
+    var extent = parameters.extent;
+    if( Array.isArray(extent) ) {
+      extent = extent.join(",")
+    }
+    query.push("geometryType=esriGeometryEnvelope&geometry=" + extent);  
+  } else if( parameters.geometry !== undefined ) { 
+    query.push("geometryType=esriGeometryPolygon&geometry=" + parameters.geometry);  
+  }
+  
+  var url = parameters.url.replace(/(Server\/\d+).*$/,'$1/query?' + query.join("&"));
   var request = CitySDK.prototype.sdkInstance.ajaxRequest(url);
 
   //Attach a completion event to the promise
